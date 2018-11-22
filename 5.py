343da9ce3a20c6303c66472cb736ed07dc10e882
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-11-22 11:38:06 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,11 @@
+number1 = int(raw_input()) 
+number2 = int(raw_input()) 
+number3 = int(raw_input()) 
+
+if (number1 > number2) and (number1 > number3):
+ biggest = num1
+elif (number2 > number1) and (number2 > number3):
+ biggest = number2
+else:
+ biggest = number3
+print(biggest)
