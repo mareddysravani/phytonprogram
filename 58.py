49f5fb1a7f1c5560f8624be963e43ee9373db478
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-11-23 21:34:33 +0530

    Update 58.py

--- 58.py
+++ 58.py
@@ -1,6 +1,6 @@
-a,b=map(int,raw_input().split()) 
+r,s=map(int,raw_input().split()) 
 list=[int(z) for z in raw_input().split()] 
-if a and b in list: 
+if r and s in list: 
      print "yes" 
 else: 
      print "no"
