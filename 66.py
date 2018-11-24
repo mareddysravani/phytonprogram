160d46ad67b0347f16f3545ddd5960f03668703a
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-11-24 12:16:25 +0530

    Update 66.py

--- 66.py
+++ 66.py
@@ -1,7 +1,7 @@
-z=int(raw_input())
-if z>0:
-    for x in range(2,z):
-        if(z%x)==0:
+s=int(raw_input())
+if s>0:
+    for x in range(2,s):
+        if(s%x)==0:
             print "no"
             break
     else:
