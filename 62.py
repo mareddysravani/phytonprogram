8f679333596e209b36c9fd9530d0e6054090d852
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-11-24 12:30:50 +0530

    Update 62.py

--- 62.py
+++ 62.py
@@ -1,10 +1,11 @@
-p=raw_input()
+r=raw_input()
 """if all(n in '12' for n in q):
     print "yes"
 else:
-    print ("no")
+    print "no"
     """
-if not(p.translate(None,'12')):
-    print ("yes")
+if not(r.translate(None,'12')):
+    print "yes"
 else:
-    print 9"no")
+    print 9"no"
+    
