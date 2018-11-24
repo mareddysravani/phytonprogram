22cdf4f211f7cbfaebd176d811d2ba37ef18b876
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-11-24 12:45:32 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,11 @@
-number1 = int(raw_input()) 
-number2 = int(raw_input()) 
-number3 = int(raw_input()) 
+num1 = int(raw_input()) 
+num2 = int(raw_input()) 
+num3 = int(raw_input()) 
 
-if (number1 > number2) and (number1 > number3):
+if (num1 > num2) and (num1 > num3):
  biggest = num1
-elif (number2 > number1) and (number2 > number3):
- biggest = number2
+elif (num2 > num1) and (num2 > num3):
+ biggest = num2
 else:
- biggest = number3
+ biggest = num3
 print(biggest)
