2cc3fc24f4bbb1f6fa44562dad981bb0954da004
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-08 11:23:18 +0530

    Create 83.py

---
+++ 83.py
@@ -0,0 +1,12 @@
+c1=int(raw_input())
+d1=int(raw_input())
+a2=int(raw_input())
+b2=int(raw_input())
+a3=int(raw_input())
+b3=int(raw_input())
+a4=int(raw_input())
+b4=int(raw_input())
+print c1 / d1
+print a2 % b2
+print a3 / b3
+print a4 % b4
