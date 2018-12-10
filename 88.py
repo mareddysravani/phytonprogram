da15f2cbb9ab63bc07271986b436f9a917acf7e3
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-10 13:06:02 +0530

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,10 +1,10 @@
-q,s=map(int,raw_input().split())
-if(q>s):
-    min=q
+r,s=map(int,raw_input().split())
+if(r>s):
+    min=r
 else:
     min=s
 while(1):
-    if(min%q==0 and min%s==0):
+    if(min%r==0 and min%s==0):
         print(min)
         break
     min=min+1
