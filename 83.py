9eb5b59d0b4f3354db12518375594dafe7d35faa
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-10 12:42:41 +0530

    Update 83.py

--- 83.py
+++ 83.py
@@ -1,12 +1,12 @@
 c1=int(raw_input())
 d1=int(raw_input())
-a2=int(raw_input())
-b2=int(raw_input())
-a3=int(raw_input())
-b3=int(raw_input())
-a4=int(raw_input())
-b4=int(raw_input())
+p2=int(raw_input())
+q2=int(raw_input())
+p3=int(raw_input())
+q3=int(raw_input())
+p4=int(raw_input())
+q4=int(raw_input())
 print c1 / d1
-print a2 % b2
-print a3 / b3
-print a4 % b4
+print p2 % q2
+print p3 / q3
+print p4 % q4
