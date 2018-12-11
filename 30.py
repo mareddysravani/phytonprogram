9ef0737336c6ae5f55693e340d9034145977a16c
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-11 11:36:43 +0530

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,4 +1,4 @@
-h1,m1=[int(x) for x in raw_input().split(" ")]
-h2,m2=[int(x) for x in raw_input().split(" ")]
-timeDiffInMinutes=ads((h1*60+m1)-(h2*60+m2))
-print(str(timeDiffInMinutes/60)+" "+str(timeDiffInMinutes%60))
+r1,s1=[int(x) for x in raw_input().split(" ")]
+r2,s2=[int(x) for x in raw_input().split(" ")]
+timeDiffInMinutes=ads((r1*60+s1)-(r2*60+s2))
+print str(timeDiffInMinutes/60)+" "+str(timeDiffInMinutes%60)
