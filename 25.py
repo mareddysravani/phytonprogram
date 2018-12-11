8f537c288e822bacda4b84733ec892a4f929e2cf
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-11 11:25:01 +0530

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,10 +1,10 @@
 import math
 def sort(values):
-  for i in range(len(values)):
-    for j in range(i,len(values)):
-      if(values[i]>values[j]):
-        temp=values[i]
-        values[j]=temp
+  for r in range(len(values)):
+    for s in range(r,len(values)):
+      if(values[r]>values[s]):
+        temp=values[r]
+        values[s]=temp
 n=int(raw_input())
 values=[int(x) for x in raw_input().split("")
 sort(values)
