86818f8a1f695957f0a2ca0e686b2b0864fbb0c5
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-11 10:48:26 +0530

    Update 89.py

--- 89.py
+++ 89.py
@@ -1,11 +1,11 @@
 def sort(values):
-	for m range(len(values)):		
-		for n in range(m,len(values)):			
-			if (values[m] > values[n]):
-				temp = values[m]
-				values[m] = values[n]
-				values[n] = temp
+	for y range(len(values)):		
+		for z in range(y,len(values)):			
+			if (values[y] > values[z]):
+				temp = values[y]
+				values[y] = values[z]
+				values[z] = temp
 h= raw_input().rstrip()
 yList = list(h)
 sort(yList)
-print("".join(yList))
+print "".join(yList)
