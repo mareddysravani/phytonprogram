827b9df64e2594293685ee208b3c181931b138ef
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-13 12:15:34 +0530

    Update 80.py

--- 80.py
+++ 80.py
@@ -1,8 +1,8 @@
-a=int(raw_input())
-b=[]
-while a!=0:
-    d=a%10
+r=int(raw_input())
+s=[]
+while r!=0:
+    d=r%10
     if d%2==1:
-        b.append(d)
-    a=a/10
-print str(b[::-1]).replace('[',"").replace(']',"")
+        s.append(d)
+    r=r/10
+print str(s[::-1]).replace('[',"").replace(']',"")
