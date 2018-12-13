c8ede725e410012699cabe0e1d490bdabefa2c8c
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-13 12:12:58 +0530

    Update 79.py

--- 79.py
+++ 79.py
@@ -1,7 +1,7 @@
 import math
-a,b=map(int,raw_input().split())
-b=a * b
-if math.sqrt(b).is_integer():
-    print "yes"
+r,s=map(int,raw_input().split())
+s=r * s
+if math.sqrt(s).is_integer():
+    print ("yes")
 else:
-    print "no"
+    print ("no")
