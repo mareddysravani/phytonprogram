bef07a06f9d7990673c9b783b3c607b5e28866dd
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2018-12-19 21:46:22 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,17 +1,17 @@
 x=int(raw_input())
-n1=1
-n2=1
+r1=1
+r2=1
 i=2
 if x<=0:
   print("positive number")
 elif x==1:
   print 1,
 else:
-  print n1,
-  print n2,
+  print r1,
+  print r2,
   while i<x:
-      n3=n1+n2
-      print n3,
-      n1=n2
-      n2=n3
+      r3=r1+r2
+      print r3,
+      r1=r2
+      r2=r3
       i=i+1
